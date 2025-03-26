71292780d113daf4f8a0e936a8a168a0021860b0
노준일 <nojun-il@nojun-il-ui-MacBookAir.local>
2025-03-26 23:19:13 +0900

    chore: complete Tailwind CSS setup

---
+++ wtafe/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // 리액트 컴포넌트를 포함한 모든 파일
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
