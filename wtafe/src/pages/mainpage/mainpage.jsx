082648031b092ad58ac7baa4638713a526ee2867
USER <user@AL02529596-10.local>
2025-04-15 16:01:00 +0900

    로그인 성공 시, 메인페이지로 이동하는 로직 구현

---
+++ wtafe/src/pages/mainpage/mainpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './mainpage.css';
+
+const MainPage = () => {
+  return (
+    <div className="mainpage-container">
+      <h1>메인 페이지</h1>
+      <p>로그인에 성공했습니다!</p>
+    </div>
+  );
+};
+
+export default MainPage;
